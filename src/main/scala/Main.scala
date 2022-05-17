27642f5234a96af4f7417e46c8bbef52d3d7ea60
Kang, Ning <heath.kang@foxmail.com>
2022-05-17 10:40:17 +0000

    update: 1. add get all files list

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,14 @@
 import org.http4s.ember.client._
 import org.http4s.client._
 import org.http4s.client.dsl.io._
-import org.http4s.Uri
-import org.http4s.Request
-import org.http4s.Status
+import org.http4s.{Uri, Request, Status, Header}
 import org.http4s.headers._
 import org.http4s.MediaType._
 import org.http4s.Method._
 import org.http4s.circe._
 import cats.effect._
+import cats.implicits._
+import cats.data.EitherT
 import io.circe._
 import io.circe.literal._
 import io.circe.generic.auto._
@@ -17,6 +17,9 @@ import io.circe.optics.JsonPath._
 import java.util.concurrent._
 import scala.concurrent.ExecutionContext
 import org.http4s.EntityEncoder
+import java.io.File
+import cats.instances.duration
+import org.http4s.syntax.header
 
 
 object MubuExporter extends IOApp.Simple {
@@ -27,10 +30,13 @@ object MubuExporter extends IOApp.Simple {
     implicit val httpClient: Client[IO] = JavaNetClientBuilder[IO].create
     val target = Uri.uri("https://api2.mubu.com/v3/api/user/phone_login")
 
-    for{
-      request <- buildRequest(target)
+    val files = for {
+      request <- EitherT.right(buildRequest(target))
       token <- getJWTtoken(request)
-    } yield println(token)
+      fileList <- getAllFiles(token)
+    } yield fileList
+
+    files.value.map(println(_))
   }
   
   def buildRequest(url: Uri): IO[Request[IO]] = {
@@ -49,67 +55,59 @@ object MubuExporter extends IOApp.Simple {
       request
   }
 
-  def getJWTtoken(request: Request[IO])(implicit client: Client[IO]): IO[String] = {
+  def getJWTtoken(request: Request[IO])(implicit client: Client[IO]): EitherT[IO, ErrorMsg, String] = {
     val login = client.run(request).use{
-      case Status.Successful(res) => res.attemptAs[Json].value
+      case Status.Successful(res) => res.attemptAs[Json].leftMap(e => s"Error happened when decode").value
       case res => res.as[String].map(b => Left(s"Request $request failed with status ${res.status.code} and body $b"))
     }
     val _tokenPath = root.data.token.string
 
-    val token = login.map( json => json match {
-      case Right(j) => _tokenPath.getOption(j).getOrElse("No token")
-      case Left(e)  => s"Error happened ${e}"
-    })
+    val token = EitherT(login).leftMap(e => ErrorMsg(e)).map( j=>
+        _tokenPath.getOption(j).getOrElse("No token"))
     token
   }
 
+  def getAllFiles(token: String)(implicit client: Client[IO]): EitherT[IO, ErrorMsg, List[String]] = {
+    val fileQuery = FileQuery("")
+    val request = for {
+      url <- Uri.fromString(RoutePath.GET_ALL_DOCUMENTS_PATH) 
+    } yield POST(fileQuery.asJson, url).putHeaders(Header("jwt-token", token))
+
+    val eitherTreq = EitherT(IO.pure(request)).leftMap(e => ErrorMsg(s"parse url ${RoutePath.GET_ALL_DOCUMENTS_PATH} error"))
+
+    val response = eitherTreq.map(req => 
+      client.run(req).use{
+        case Status.Successful(res) => res.attemptAs[Json].leftMap(e => s"Decode File list error").value
+        case res => res.as[String].map(e => Left(s"Get file list wrong response ${res.status.code} and body $e"))
+    }).flatMap( res => {
+      val _fileListPath = root.data.documents.each.id.string
+      EitherT(res).leftMap(e => ErrorMsg(e)).map(value => {
+         _fileListPath.getAll(value)
+      })
+    })
+
+    response
+  }
   case class Payload(
     callbackType: String,
     phone: String,
     password: String
   )
 
-  // case class AuthResponse(
-  //   code: String,
-  //   data: DataResponse
-  // )
-  
-  // case class DataResponse(
-  //   googleId: String,
-  //   gender: String,
-  //   year: String,
-  //   city: String,
-  //   remark: String,
-  //   view: String,
-  //   province: String,
-  //   googleName: String,
-  //   id: String,
-  //   wxName: String,
-  //   email: String,
-  //   vipEndDate: String,
-  //   clientId: String,
-  //   level: String,
-  //   encryptPassword: String,
-  //   facebookId: String,
-  //   photo: String,
-  //   updateTime: String,
-  //   wxId: String,
-  //   passSecure: String,
-  //   sort: String,
-  //   appleId: String,
-  //   qqId: String,
-  //   appleName: String,
-  //   agreeTermService: String,
-  //   token: String,
-  //   phone: String,
-  //   createTime: String,
-  //   name: String,
-  //   anonymUserFlag: String,
-  //   qqName: String,
-  //   toutiaoId: String,
-  //   facebookName: String,
-  //   larkId: String,
-  // )
+  case class FileQuery(
+    start: String = ""
+  )
+
+  case class ErrorMsg(
+    msg: String
+  )
+}
+
+object RoutePath {
+  val GET_SINGLE_DOC_PATH = "https://api2.mubu.com/v3/api/document/edit/get"
+  val EXPORT_SINGLE_DOC_PATH = "https://mubu.com/convert/export"
+  val PHONE_LOGIN_PATH = "https://api2.mubu.com/v3/api/user/phone_login"
+  val GET_ALL_DOCUMENTS_PATH = "https://api2.mubu.com/v3/api/list/get_all_documents_page"
 }
 
 
