16778a01b9b9fbd27f176b493683053e8e49b6d4
Kang, Ning <heath.kang@foxmail.com>
2022-05-19 09:52:38 +0000

    refactor: 1. use tagless-final pattern;

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,6 +8,7 @@ import org.http4s.Method._
 import org.http4s.circe._
 import cats.effect._
 import cats.implicits._
+import cats._
 import cats.data.EitherT
 import io.circe._
 import io.circe.literal._
@@ -23,20 +24,22 @@ import org.http4s.syntax.header
 
 
 object MubuExporter extends IOApp.Simple {
+  import Model._
+  import Mubu._
 
   def run: IO[Unit] = {
-
+    
     val blockingEC = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(5))
     implicit val httpClient: Client[IO] = JavaNetClientBuilder[IO].create
     val target = Uri.uri("https://api2.mubu.com/v3/api/user/phone_login")
+    val mubuClient = Mubu.MubuInterpreter.make(httpClient)
 
     val files = for {
-      request <- EitherT.right(buildRequest(target))
-      token <- getJWTtoken(request)
-      fileList <- getAllFiles(token)
-    } yield fileList
+      request <- buildRequest(target)
+      token <- mubuClient.getJWTtoken(request)
+    } yield println(token)
 
-    files.value.map(println(_))
+    files
   }
   
   def buildRequest(url: Uri): IO[Request[IO]] = {
@@ -88,6 +91,71 @@ object MubuExporter extends IOApp.Simple {
 
     response
   }
+
+  // def getSingleDoc(path: String)(implicit client: Client[IO])
+
+
+}
+
+object RoutePath {
+  val GET_SINGLE_DOC_PATH = "https://api2.mubu.com/v3/api/document/edit/get"
+  val EXPORT_SINGLE_DOC_PATH = "https://mubu.com/convert/export"
+  val PHONE_LOGIN_PATH = "https://api2.mubu.com/v3/api/user/phone_login"
+  val GET_ALL_DOCUMENTS_PATH = "https://api2.mubu.com/v3/api/list/get_all_documents_page"
+}
+
+object Mubu {
+  import Model._
+
+  type EitherMsg[A] = Either[ErrorMsg, A]
+  trait Mubu[F[_]] {
+    def getJWTtoken(request: Request[F]): F[EitherMsg[String]]
+    def getAllFilesList(request: Request[F]): F[EitherMsg[List[String]]]
+    def getSingleDocContent(request: Request[F]): F[EitherMsg[String]]
+  }
+
+  trait RequestBuilder[F[_]] {
+    def buildJWTRequest(url: String): F[Request[F]]
+    def buildGetAllFilesListRequest(token: String): F[Request[F]]
+    def buildGetSingleDocContentRequest(token: String, fileId: String): F[Request[F]]
+  }
+
+  class RequestBuilderInterpreter[F[_]: Async] 
+    extends RequestBuilder[F] {
+      override def buildJWTRequest(url: String): F[Request[F]] = ???
+      override def buildGetAllFilesListRequest(token: String): F[Request[F]] = ???
+      override def buildGetSingleDocContentRequest(token: String, fileId: String): F[Request[F]] = ???
+    }
+ 
+
+  object MubuInterpreter{
+    def make[F[_]:Async](client: Client[F]): MubuInterpreter[F] = {
+      new MubuInterpreter(client)
+    }
+  }
+  class MubuInterpreter[F[_]: Async] private(client: Client[F])
+    extends Mubu[F] {
+      override def getJWTtoken(request: Request[F]): F[EitherMsg[String]] = {
+        val login: F[Either[String, Json]] = client.run(request).use {
+          case Status.Successful(res) => res.attemptAs[Json].leftMap(e => s"Decode File list error").value
+          case res => res.as[String].map(b => Left(s"Request $request failed with status ${res.status.code} and body $b"))
+        }
+        val _tokenPath = root.data.token.string
+
+        val token = EitherT(login).leftMap(e => ErrorMsg(e)).map( j=>
+            _tokenPath.getOption(j).getOrElse("No token"))
+
+        token.value
+      }
+
+      override def getAllFilesList(request: Request[F]): F[EitherMsg[List[String]]]= ???
+
+      override def getSingleDocContent(request: Request[F]): F[EitherMsg[String]]= ???
+  }
+
+}
+
+object Model{
   case class Payload(
     callbackType: String,
     phone: String,
@@ -102,13 +170,5 @@ object MubuExporter extends IOApp.Simple {
     msg: String
   )
 }
-
-object RoutePath {
-  val GET_SINGLE_DOC_PATH = "https://api2.mubu.com/v3/api/document/edit/get"
-  val EXPORT_SINGLE_DOC_PATH = "https://mubu.com/convert/export"
-  val PHONE_LOGIN_PATH = "https://api2.mubu.com/v3/api/user/phone_login"
-  val GET_ALL_DOCUMENTS_PATH = "https://api2.mubu.com/v3/api/list/get_all_documents_page"
-}
-
 
 
